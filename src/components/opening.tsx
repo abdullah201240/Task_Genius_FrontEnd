a74a89e3021f2adc2ef1e6f84b7bc6bb0d180951
abdullah201240 <abdullahalsakib7075@gmail.com>
2024-03-17 16:40:45 +0600

    2nd update from sakib

---
+++ src/components/opening.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Opening = () => {
+  return (
+    <View>
+      <Text>Opening</Text>
+    </View>
+  )
+}
+
+export default Opening
+
+const styles = StyleSheet.create({})
